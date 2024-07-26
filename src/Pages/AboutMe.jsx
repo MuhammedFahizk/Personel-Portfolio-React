a7db2c4f37cbe408187daed6732eb375bfe47bdd
fahiz <fahizk100@gmail.com>
2024-07-26 14:45:56 +0530

    add motion for hero

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -3,13 +3,15 @@ import { useEffect } from "react";
 import Nav from "../Component/Nav";
 import Skill from "../Pages/Skill.jsx";
 import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
-import {  motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Education from "../Component/Education.jsx";
 import Contact from "../Component/Contact.jsx";
+
 const AboutMe = () => {
   useEffect(() => {
     // Simulate fetching data from an API or data file
   }, []);
+
   const icons = [
     {
       icon: FaGithub,
@@ -32,67 +34,140 @@ const AboutMe = () => {
       color: '#34af23'
     },
   ];
+
+  const imageVariants = {
+    offscreen: {
+      scale: 0.8,
+      opacity: 0,
+    },
+    onscreen: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
+      },
+    },
+  };
+
+  const containerVariants = {
+    offscreen: {
+      opacity: 0,
+      y: 50,
+    },
+    onscreen: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.3,
+        duration: 1,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const contentVariants = {
+    offscreen: {
+      opacity: 0,
+      x: -50,
+    },
+    onscreen: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.3,
+        duration: 1,
+      },
+    },
+  };
+
+  const iconVariants = {
+    offscreen: {
+      scale: 0.8,
+      opacity: 0,
+      y: 300,
+    },
+    onscreen: {
+      y: -10,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
+      },
+    },
+  };
+
   return (
     <>
       <Nav />
-      <div className="my-10">
-        <div className="text-white relative">
-          <img
-            className="absolute  inset-0 w-full h-full object-cover"
+      <motion.div
+        className="my-10"
+        initial="offscreen"
+        animate="onscreen"
+        variants={containerVariants}
+      >
+        <motion.div className="text-white relative" variants={containerVariants}>
+          <motion.img
+            initial={{ opacity: 0, y: 300 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            className="absolute inset-0 w-full h-full object-cover"
             src="https://images.pexels.com/photos/41949/earth-earth-at-night-night-lights-41949.jpeg?auto=compress&cs=tinysrgb&"
             alt=""
           />
-          <div className="bg-black bg-opacity-50 relative w-full h-full">
-            <div className="flex flex-col  md:flex-row p-4 md:px-20 md:py-10 gap-4 md:gap-8">
-              <div className="rounded-3xl md:items-center shadow-md  md:flex  hidden sm:block w-full md:w-1/5 lg:w-1/4 h-[410px]  object-cover">
-                <img
-                  className=" relative  rounded-3xl shadow-md"
+          <motion.div className="bg-black bg-opacity-50 relative w-full h-full" variants={contentVariants}>
+            <div className="flex flex-col md:flex-row p-4 md:px-20 md:py-10 gap-4 md:gap-8">
+              <div className="rounded-3xl md:items-center shadow-md md:flex hidden sm:block w-full md:w-1/5 lg:w-1/4 h-[410px] object-cover">
+                <motion.img
+                  variants={imageVariants}
+                  className="relative rounded-3xl shadow-md"
                   src="../images/HeroImage.jpg"
                   alt="Profile"
                 />
-                <div className="absolute start-[300px] rounded p-1 flex flex-col gap-3  bg-[#080808bb] ">
+                <div className="absolute start-[300px] rounded p-1 flex flex-col gap-3 bg-[#080808bb]">
                   {icons.map((icon, index) => (
-                  <motion.div
-                  key={index}
-                    whileHover={{ scale: 1.2, rotate: 45 }}
-                    whileTap={{
-                      scale: 0.8,
-                      rotate: -12,
-                      borderRadius: "100%",
-                    }}
-                  >
+                    <motion.div
+                      key={index}
+                      whileHover={{ scale: 1.2, rotate: 45 }}
+                      whileTap={{
+                        scale: 0.8,
+                        rotate: -12,
+                        borderRadius: "100%",
+                      }}
+                    >
                       <a
-                      href={icon.link}
-                      target="_blank"
-                      rel="noreferrer"
+                        href={icon.link}
+                        target="_blank"
+                        rel="noreferrer"
                       >
                         <icon.icon
-                        size={30}
-                        style={{color: icon.color,}}
-                        className={`text-['#000] hover:text-[${icon.color}] transition duration-300
-                        ease-in-out`}
+                          size={30}
+                          style={{ color: icon.color }}
+                          className={`text-['#000] hover:text-[${icon.color}] transition duration-300 ease-in-out`}
                         />
                       </a>
-
-                  </motion.div>
-                    ))}
-                  {/* <FaInstagram className="text-2xl text-[#cd486b]" />
-                  <FaLinkedin className="text-2xl text-[#0762C8]  " />
-                  <FaGithub className="text-2xl" />
-                  <FaWhatsapp className="text-2xl text-[#25D366]" /> */}
+                    </motion.div>
+                  ))}
                 </div>
               </div>
-              <div className="bg-gray-800 bg-opacity-80 p-6 rounded-3xl h-fit w-full md:w-1/2 lg:w-2/3">
+              <motion.div
+                className="bg-gray-800 bg-opacity-80 p-6 rounded-3xl h-fit w-full md:w-1/2 lg:w-2/3"
+                variants={contentVariants}
+              >
                 <h1 className="text-4xl font-bold mb-4">About Me</h1>
                 <p>
                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hello!
-                  My name is Muhammed Fahiz k, and I'm a MERN Stack Devoplepor
+                  My name is Muhammed Fahiz k, and I'm a MERN Stack Developer
                   from Calicut. I graduated with a BCA from the University of
-                  calicut, where I built a strong foundation in computer science
+                  Calicut, where I built a strong foundation in computer science
                   and software development.
                   <br />
                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Over
-                  the past 8 Month, I've been passionately honing my skills in
+                  the past 8 Months, I've been passionately honing my skills in
                   the MERN stack (MongoDB, Express.js, React, Node.js) under the
                   guidance of an experienced mentor. This intensive practice has
                   allowed me to develop a deep understanding of full-stack
@@ -111,9 +186,7 @@ const AboutMe = () => {
                   Feel free to reach out to me at Fahizk100@gmail.com or connect
                   with me on{" "}
                   <a
-                    href={
-                      "https://www.linkedin.com/in/muhammed-fahiz-96726b287/"
-                    }
+                    href="https://www.linkedin.com/in/muhammed-fahiz-96726b287/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-blue-500"
@@ -122,16 +195,16 @@ const AboutMe = () => {
                   </a>
                   . I'm always open to new opportunities and collaborations.
                 </p>
-              </div>
+              </motion.div>
             </div>
-          </div>
+          </motion.div>
+        </motion.div>
+        <div className="flex justify-start">
+          <Education />
         </div>
-        <div className="flex justify-start    ">
-        <Education/>
-        </div>
-      </div>
+      </motion.div>
       <Skill />
-      <Contact/>
+      <Contact />
     </>
   );
 };
